5a31bfa63a90dfb6402af001493187ce2d12b78d
Hobey Kuhn <hkuhn@umich.edu>
2013-10-06 16:59:54 -0400

    began face tracking. added training data.

--- src/tracking/recording_tracking_main.cpp
+++ src/tracking/recording_tracking_main.cpp
@@ -13,6 +13,8 @@
 // HEADER FILES
 /////////////////////////////////////////////
 #include "recording_tracking_main.h"
+#include "helper.hpp"
+#include "eigenfaces.hpp"
 
 
 
@@ -22,6 +24,8 @@
 
 // C++ Reference
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <string>
 
 // OpenCV
@@ -39,6 +43,9 @@ using namespace cv;
 
 void recording_tracking_main(const char* path) {
     
+    //
+    // LOCATE VIDEO FEED
+    //
     // retrieve feed
     cout << "Attempting to locate video..." << endl;
     VideoCapture feed(path);
@@ -47,13 +54,59 @@ void recording_tracking_main(const char* path) {
     if ( !feed.isOpened() ) {
         cout << "Cannot open the video file" << endl;
         return;
-    }
-    
+    }       
+    cout << "Video found" << endl;
     // retrieve frames per second
     double fps = feed.get(CV_CAP_PROP_FPS);
     
+    
+    //
+    // BUILD FACE TRACKER
+    //    
+    vector<Mat> images;
+	vector<int> labels;
+    string data_path;
+    
+    cout << "Building face detection model..." << endl;
+    cout << "Path to data: ";
+    cin >> data_path;
+    
+	// path to your CSV
+	string fn_csv = string(data_path);
+	// read in the images
+	try {
+		read_csv(fn_csv, images, labels);
+	} catch(exception& e) {
+		cerr << "Error opening file \"" << fn_csv << "\"." << endl;
+		exit(1);
+	}
+    
+	// get width and height
+	int width = images[0].cols;
+	int height = images[0].rows;
+    
+	// get test instances
+	Mat testSample = images[images.size()-1];
+	int testLabel = labels[labels.size()-1];
+	// ... and delete last element
+	images.pop_back();
+	labels.pop_back();
+    cout << "Data loaded successfully. Please specify number of components: ";
+    int num_components = 80; // default
+    cin >> num_components;
+	// num_components eigenfaces
+	// int num_components = 80;
+	// compute the eigenfaces
+    cout << "Building eigenfaces model..." << endl;
+	Eigenfaces eigenfaces(images, labels, num_components);    
+
+    
+    
+    //
+    // RUN FACE TRACKING ON VIDEO FEED
+    //
     // display information
-    cout << "Video found. Press esc to quit program." << endl;
+    cout << "Press esc to quit program." << endl;
     
     namedWindow("Pupil Tracking",CV_WINDOW_AUTOSIZE);
     
@@ -71,6 +124,7 @@ void recording_tracking_main(const char* path) {
             break;
         }
         
+        
         imshow("Video", frame); //show the frame in "MyVideo" window
         
         if(waitKey(30) == 27) //wait for 'esc' key press for 30 ms. If 'esc' key is pressed, break loop
@@ -84,6 +138,27 @@ void recording_tracking_main(const char* path) {
     return;
 }
 
+
+
+
+void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels) {
+	ifstream file(filename.c_str(), ifstream::in);
+	if(!file)
+		throw std::exception();
+	string line, path, classlabel;
+	// for each line
+	while (std::getline(file, line)) {
+		// get current line
+		stringstream liness(line);
+		// split line
+		getline(liness, path, ';');
+		getline(liness, classlabel);
+		// push pack the data
+		images.push_back(imread(path,0));
+        Mat img = imread(path,0);
+		labels.push_back(atoi(classlabel.c_str()));
+	}
+}
 
 
 
--- src/tracking/recording_tracking_main.h
+++ src/tracking/recording_tracking_main.h
@@ -11,12 +11,13 @@
 #define ____recording_tracking_main__
 
 #include <iostream>
+#include "opencv2/highgui/highgui.hpp"
 
-
+using namespace cv;
 
 // FUNCTION DECLARATIONS
 void recording_tracking_main(const char* path);
-
+void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels);
 
 
 #endif /* defined(____recording_tracking_main__) */
